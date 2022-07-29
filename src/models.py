60a67a8d677e079f9968c05414a2bf444044e014
ManuelCebreiro <cebreirom@gmail.com>
2022-07-29 17:04:30 +0000

    listo

--- src/models.py
+++ src/models.py
@@ -8,31 +8,47 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+class User(Base):
+    __tablename__ = 'User'
+    id = Column(Integer, ForeignKey('Follower.user_from_id'), primary_key=True)
+    username = Column(String(250))
+    firstname = Column(String(250))
+    lastname = Column(String(250))
+    email = Column(String(250))
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+
+
+class Media(Base):
+    __tablename__ = 'Media'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String(250))
+    type = Column(String(250))
+    url= Column(String(250), nullable=False)
+    post_id = Column(Integer, ForeignKey('person.id'))
+
+class Comment(Base):
+    __tablename__ = 'Comment'
+    id = Column(Integer, primary_key=True)
+    comment = Column(String(250))
+    autor_id = Column(String(250),ForeignKey('User.UserId'))
+    post_id = Column(String(250),ForeignKey('Post.id'))
+    
+    
+
+class Post(Base):
+    __tablename__ = 'Post'
+    id = Column(Integer, ForeignKey('Media.id'), primary_key=True)
+    user_id = Column(String(250), ForeignKey('User.id'))
+    
+
+class Follower(Base):
+    __tablename__ = 'Follower'
+    user_from_id = Column(Integer, primary_key=True)
+    user_id = Column(String(250), nullable=False)
 
     def to_dict(self):
         return {}
 
 ## Draw from SQLAlchemy base
-try:
-    result = render_er(Base, 'diagram.png')
-    print("Success! Check the diagram.png file")
-except Exception as e:
-    print("There was a problem genering the diagram")
-    raise e
+
+render_er(Base, 'diagram.png')
